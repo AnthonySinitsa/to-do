cf35eb0319f4761ccfd8f46a59bb62c7f92830e0
AnthonySinitsa <itonyemail@gmail.com>
2023-07-20 23:45:45 +0000

    command

--- todo/models.py
+++ todo/models.py
@@ -6,9 +6,9 @@ class Todo(models.Model):
   title = models.CharField(max_length=64)
   body = models.TextField(default='1. ')
   author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-  
+
   def __str__(self):
     return self.title
-  
+
   def get_absolute_url(self):
     return reverse('todo_detail', args=[str(self.id)])
