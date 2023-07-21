c8e95cf3186952a762b709c683522c471a659aeb
AnthonySinitsa <itonyemail@gmail.com>
2023-07-21 01:19:27 +0000

    command

--- reviews/models.py
+++ reviews/models.py
@@ -3,11 +3,12 @@ from django.contrib.auth import get_user_model
 from django.urls import reverse
 
 class Review(models.Model):
+  title = models.CharField(max_length=64, default='review')
+  body = models.TextField(default='review')
   author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-  body = models.TextField(default='good product:/')
   
   def __str__(self):
-    return self.author
+    return self.title
   
   def get_absolute_url(self):
-    return reverse('reviews', args=[str(self.id)])
+    return reverse('review_detail', args=[str(self.id)])
--- reviews/views.py
+++ reviews/views.py
@@ -3,24 +3,25 @@ from .models import Review
 from django.urls import reverse_lazy
 
 class ReviewListView(ListView):
-  template_name = 'review-list.html'
+  template_name = 'reviews-list.html'
   model = Review
+  context_object_name = 'reviews'
   
 class ReviewDetailView(DetailView):
-  template_name = 'review-detail.html'
+  template_name = 'reviews-detail.html'
   model = Review
   
 class ReviewCreateView(CreateView):
-  template_name = 'review-create.html'
+  template_name = 'reviews-create.html'
   model = Review
   fields = ['title', 'body', 'author']
   
 class ReviewUpdateView(UpdateView):
-  template_name = 'review-update.html'
+  template_name = 'reviews-update.html'
   model = Review
   fields = ['id', 'title', 'body', 'author']
   
 class ReviewDeleteView(DeleteView):
-  template_name = 'review-delete.html'
+  template_name = 'reviews-delete.html'
   model = Review
   success_url = reverse_lazy('review_list')
